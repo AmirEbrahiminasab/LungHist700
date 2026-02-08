44b6a7b0f065b419f92c2c7ddb03f2c207bc6e06
AmirEbrahiminasab <ebrahiminasab82@gmail.com>
2026-02-08 11:32:30 +0330

    fixed Batch Norm issue

--- HistoLib/traintest.py
+++ HistoLib/traintest.py
@@ -20,7 +20,7 @@ def make_backbone_trainable(model, trainable=True):
         # We want to keep the BatchNormalization layers trainable for statistics, 
         # but freeze the weights of Convolutions during warm-up.
         if isinstance(layer, tf.keras.layers.BatchNormalization):
-            layer.trainable = True
+            layer.trainable = False
         elif 'dense' in layer.name or 'dropout' in layer.name:
             layer.trainable = True
         else:
