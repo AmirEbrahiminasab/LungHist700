faa561858d0dbaf5530fe78da2aa7e4e9a2778e5
AmirEbrahiminasab <ebrahiminasab82@gmail.com>
2026-02-09 12:40:26 +0330

    fixed efficientnetb3 model issue

--- HistoLib/models.py
+++ HistoLib/models.py
@@ -35,11 +35,13 @@ def efficientnet_b3_model(num_classes, input_shape):
     Creates a model with an EfficientNet-B3 backbone.
     """
     inputs = layers.Input(input_shape)
+    inputs = layers.Rescaling(scale=255.0)(inputs)
 
     base_model = EfficientNetB3(include_top=False, weights='imagenet', pooling='avg', input_tensor=inputs)
 
-    x = layers.Dense(256, activation='relu', name='prev_dense')(base_model.output)
-    x = layers.Dropout(0.5, name='dropout')(x)
+    x = layers.Dense(128, activation='relu', name='prev_dense')(base_model.output)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dropout(0.6, name='dropout')(x)
     outputs = layers.Dense(num_classes, activation='softmax', name='last_dense')(x)
 
     model = Model(inputs=inputs, outputs=outputs)
